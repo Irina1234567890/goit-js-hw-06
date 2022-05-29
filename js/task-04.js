cb76f9f7db128f3aa747cddffe2953e5db7b75b4
Irina1234567890 <irina3213@gmail.com>
2022-05-29 22:36:56 +0300

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,24 @@
 /*Создай переменную counterValue в которой будет храниться текущее значение счетчика и инициализируй её значением 0.
 Добавь слушатели кликов на кнопки, внутри которых увеличивай или уменьшай значение счтетчика.
 Обновляй интерфейс новым значением переменной counterValue.*/
+
+const counter = document.querySelector('#value');
+let counterValue = 0;
+
+const decrementEl = document.querySelector('button[data-action="decrement"]');
+decrementEl.addEventListener('click', () => {
+  counterValue -=1;
+  counter.textContent = counterValue;
+});
+
+const incrementEl = document.querySelector('button[data-action="increment"]');
+incrementEl.addEventListener('click', () => {
+  counterValue +=1;
+  counter.textContent = counterValue;
+});
+
+
+
+
+
+
