52f5b59afc2c89fa0398a2893d2a6f76e81e63f2
Irina1234567890 <irina3213@gmail.com>
2022-05-30 22:29:28 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+const formEl = document.querySelector('login-form');
+const mailEl = document.querySelector('email');
+const pasEl = document.querySelector('password');
+const btnEl = document.querySelector('submit');
+
+formEl.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { login, password }
+  } = event.currentTarget;
+
+  if (login.value === "" || password.value === "") {
+    alert('All fields must be filled!');
+  }
+
+  console.log(`Login: ${login.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
