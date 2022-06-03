5a0674da8022f8e321cc088c3a1ed9bf003e02f7
Irina1234567890 <irina3213@gmail.com>
2022-06-03 10:20:14 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -7,15 +7,60 @@ const ingredients = [
   'Condiments',
 ];
 
-for (const ingredient of ingredients) {
+for (let ingredient of ingredients) {
   const ingredientEl = document.createElement('li');
-ingredientEl.textContent = `${ingredient}`;
-ingredientEl.classList.add('item');
+  ingredientEl.textContent = `${ingredient}`;
+  ingredientEl.classList.add('item');
 
 const ingredientsList = document.querySelector('#ingredients');
-ingredientsList.appendChild(ingredientEl);
+ingredientsList.append(ingredientEl);
+
 console.log(ingredientsList);
-}
+};
+// const ingredientsEl = document.querySelector('#ingredients');
+
+// for (let ingredient of ingredients) {
+//   const ingredientEl = document.createElement('li');
+//   ingredientEl.textContent = `${ingredient}`;
+//   ingredientEl.classList.add('item');
+
+// const ingredientsList = document.querySelector('#ingredients');
+// ingredientsEl.appendChild(...ingredientsList);
+// console.log(ingredientsList);
+// };
+
+// ingredientsEl.appendChild(...ingredientsList);
+// console.log(ingredientsList);
+
+
+// const ingredientsEl = document.querySelector('#ingredients');
+
+// const makeIngredients = (options) => {
+//   return options.map(option => {
+//       const ingredientEl = document.createElement('li');
+//       ingredientEl.textContent = `${ingredient}`;
+//       ingredientEl.classList.add('item');
+
+//       return ingredientEl;
+//   });
+// };
+
+// const elements = makeIngredients(ingredients);
+// ingredientsEl.append(...elements);
+
+
+// const elements = ingredients.map(option => {
+//   const ingredientEl = document.createElement('li');
+//   ingredientEl.textContent = `${ingredient}`;
+//   ingredientEl.classList.add('item');
+
+//   return ingredientEl;
+// });
+ 
+// ingredientsEl.appendChild(...elements);
+// console.log(elements);
+
+
 
 /*Напиши скрипт, который для каждого элемента массива ingredients:
 
