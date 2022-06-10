998db82c1610236412c4daa5d335e1aae05251a2
Irina1234567890 <irina3213@gmail.com>
2022-06-10 12:12:56 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -3,10 +3,15 @@
 // Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
 // Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
-const formEl = document.querySelector('.login-form');
-const mailEl = document.querySelector('email');
-const pasEl = document.querySelector('password');
-const btnEl = document.querySelector('submit');
+const submitForm = document.querySelector('.login-form');
+// const mailEl = document.querySelector('email');
+// const pasEl = document.querySelector('password');
+// const btnEl = document.querySelector('submit');
+
+const submitButton = document.querySelector('button');
+const emailField = document.querySelector('input[type="email"]').value;
+const passwordField = document.querySelector('input[type="password"]').value;
+
 
 // formEl.addEventListener('submit', handleSubmit);
 // console.log(formEl);
@@ -25,7 +30,7 @@ const btnEl = document.querySelector('submit');
 let user = {
   email: '',
   password: '',
-},
+};
 
 submitForm.addEventListener('submit', () => {
   if(emailField || passwordField === '') {
